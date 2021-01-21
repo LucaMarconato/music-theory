84c52df886fd15a3cf545ae57f25a66685ea97da
Macbook Marconato <m.lucalmer@gmail.com>
2021-01-21 23:56:23 +0100

    fixed octave, working on inversions

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ from functools import partial
 import rtmidi
 import time
 
+
 # import music21
+
 
 class PathGrabber:
     def __init__(self):
@@ -19,8 +21,8 @@ class PathGrabber:
         self.path = path
 
 
-n1 = note.Note('C4', quarterLength=1)
-n2 = note.Note('A4', quarterLength=1)
+n1 = note.Note("C4", quarterLength=1)
+n2 = note.Note("A4", quarterLength=1)
 s = stream.Stream()
 s.append(n1)
 s.append(n2)
@@ -32,42 +34,60 @@ s.append(n2)
 # c_minor.show('test.svg')
 
 
+def get_minor_chors(random_inversion=True):
+    chords_description = [
+        ["C4", "E4-", "G4"],
+        ["C4#", "E4", "G4#"],
+        ["D4-", "F4-", "A4-"],
+        ["D4", "F4", "A4"],
+        ["D4#", "F4#", "A4#"],
+        ["E4-", "G4-", "B4-"],
+        ["E4", "G4", "B4"],
+        ["F4", "A4-", "C5"],
+        ["F4#", "A4", "C5#"],
+        ["G4-", "B4--", "D5-"],
+        ["G4", "B4-", "D5"],
+        ["G4#", "B4", "D5#"],
+        ["A4-", "C5-", "E5-"],
+        ["A4", "C5", "E5"],
+        ["A4#", "C5#", "E5#"],
+        ["B4-", "D5-", "F5"],
+        ["B4", "D5", "F5#"],
+    ]
+    chords = [chord.Chord(c) for c in chords_description]
+    # for c in chords:
+    #     for note in c:
+    #         note.octave = 5
+    for c in chords:
+        if not random_inversion:
+            c.inversion(0)
+        else:
+            c.inversion(random.randint(0, 2))
+    for (root, _, _), c in zip(chords_description, chords):
+        should_be = f'{root[0] + root[2:].replace("-", "b")}-minor triad'
+        assert c.pitchedCommonName == should_be
+    return chords
+
+
+minor_chords = get_minor_chors()
+
+
 def chord_training(chord_type, current_stack):
     # address = chord.tables.ChordTableAddress(3, 11, -1, 2)
     # iv = chord.tables.addressToIntervalVector(address)
     # my_chord = chord.fromIntervalVector(iv)
     # print(type(my_chord))
     # print(my_chord.pitchedCommonName)
-    minor_chords = [
-        ['C', 'E#', 'G'],
-        ['C#', 'E', 'G#'],
-        ['D-', 'F-', 'A-'],
-        ['D', 'F', 'A'],
-        ['D#', 'F#', 'A#'],
-        ['E-', 'G-', 'B-'],
-        ['E', 'G', 'B'],
-        ['F', 'A-', 'C'],
-        ['F#', 'A', 'C#'],
-        ['G-', 'B--', 'D-'],
-        ['G', 'B-', 'D'],
-        ['G#', 'B', 'D#'],
-        ['A-', 'C-', 'E-'],
-        ['A', 'C', 'E'],
-        ['A#', 'C#', 'E#'],
-        ['B-', 'D-', 'F'],
-        ['B', 'D', 'F#']
-    ]
-    random_chord = chord.Chord(random.choice(minor_chords))
-    random_chord.inversion(0)
+    random_chord = random.choice(minor_chords)
 
     # chord.chordTables.Chor(address)
 
     # a = f'C{np.random.random_integers(4, 7, 1).item()}'
     # c_minor = chord.Chord([a, 'G4', 'E-5'])
-    random_chord.duration.type = 'whole'
+    random_chord.duration.type = "whole"
     chord_name = random_chord.pitchedCommonName
     path_grabber = PathGrabber()
-    random_chord.show('lily.png', my_show=False, my_path_grabber=path_grabber)
+    random_chord.show("lily.png", my_show=False, my_path_grabber=path_grabber)
     im = Image.open(path_grabber.path)
     x = np.array(im)
     black_pixels_012 = np.where(x != [255, 255, 255])
@@ -83,47 +103,57 @@ def chord_training(chord_type, current_stack):
     plt.box(False)
     ax.set_xticks([])
     ax.set_yticks([])
+    print(chord_name)
     ax.set_title(chord_name)
     ax.imshow(x)
     plt.draw()
     plt.pause(0.001)
     correct_pitches = set()
-    for c in random_chord.pitches:
-        correct_pitches.add(c.pitchClass)
+    for c in random_chord._notes:
+        correct_pitches.add(c.pitch.pitchClass + 12 * c.octave)
     while True:
         time.sleep(0.03)
-        current_stack_no_octave = set([c % 12 for c in current_stack])
+        clone_stack = current_stack.copy()
+        current_stack_no_octave = set([c % 12 for c in clone_stack])
         # print(current_stack_no_octave, correct_pitches)
-        if current_stack_no_octave == correct_pitches:
+        if len(clone_stack) > 0:
+            print('ehila')
+            pass
+        if clone_stack == correct_pitches:
             ax.imshow(green_x)
             plt.draw()
             plt.pause(0.001)
             time.sleep(1)
-            print('CORRECT!')
+            print("CORRECT!")
             break
         # else:
         #     print('NOT CORRECT')
     # input("Press [enter] to continue.")
 
 
-Mode = Enum('Mode', 'chord interval scale key')
-ChordType = Enum('ChordType', 'triad extended_triad seventh')
+Mode = Enum("Mode", "chord interval scale key")
+ChordType = Enum("ChordType", "triad extended_triad seventh")
 
 
 def my_inquirer(questions):
     import termios
+
     try:
         answers = inquirer.prompt(questions)
-        return answers['answer']
+        return answers["answer"]
     except termios.error as a:
-        if a == (25, 'Inappropriate ioctl for device'):
+        if a == (25, "Inappropriate ioctl for device"):
             pass
     return questions[0].choices[0]
 
 
 def ask_mode():
     questions = [
-        inquirer.List('answer', message='what do you want to practice?', choices=[m.name for m in Mode])
+        inquirer.List(
+            "answer",
+            message="what do you want to practice?",
+            choices=[m.name for m in Mode],
+        )
     ]
     answer = my_inquirer(questions)
     return answer
@@ -131,13 +161,14 @@ def ask_mode():
 
 def ask_chord_type():
     questions = [
-        inquirer.List('answer', message='which chord type do you want to practice?',
-                      choices=[m.name for m in ChordType])
+        inquirer.List(
+            "answer",
+            message="which chord type do you want to practice?",
+            choices=[m.name for m in ChordType],
+        )
     ]
     answer = my_inquirer(questions)
     return answer
-
-
 
 
 # the second argument can be any python object which is passed by the calling function to make possible to access
@@ -147,27 +178,36 @@ def midi_in_callback(value, current_stack):
     # key down
     if value[0][0] == 144:
         pitch = value[0][1]
+        # problem with my keyboard?
+        pitch -= 12
         assert pitch not in current_stack
         current_stack.add(pitch)
     # key up
     if value[0][0] == 128:
         pitch = value[0][1]
+        # problem with my keyboard?
+        pitch -= 12
         assert pitch in current_stack
         current_stack.remove(pitch)
     # print(current_stack)
 
-if __name__ == '__main__':
-    print('available ports:')
+
+if __name__ == "__main__":
+    print("available ports:")
     print()
-    print('\tInput:')
-    print('\t', 'ID', 'Name', sep='\t')
+    print("\tInput:")
+    print("\t", "ID", "Name", sep="\t")
     l = []
     for i in range(rtmidi.MidiIn().get_port_count()):
         s = rtmidi.MidiIn().get_port_name(i)
         print(i, s)
         l.append(i)
-    assert len(l) > 0
-    print('using the first midi device')
+    if len(l) == 0:
+        print("no midi devices detected!")
+        import os
+
+        os._exit(0)
+    print("using the first midi device")
     current_stack = set()
     in_port = rtmidi.MidiIn()
     in_port.open_port(l[0])
@@ -175,14 +215,14 @@ if __name__ == '__main__':
     exit = False
     while not exit:
         mode = ask_mode()
-        if mode == 'chord':
+        if mode == "chord":
             chord_type = ask_chord_type()
             trainer = partial(chord_training, chord_type)
-        elif mode == 'interval':
+        elif mode == "interval":
             pass
-        elif mode == 'scale':
+        elif mode == "scale":
             pass
-        elif mode == 'key':
+        elif mode == "key":
             pass
         else:
             raise ValueError()
@@ -190,5 +230,5 @@ if __name__ == '__main__':
             while True:
                 trainer(current_stack)
         except KeyboardInterrupt:
-            print('back to main menu!')
+            print("back to main menu!")
             pass
