f4fe7d57db08646bdb3ba63e6b1442944a24e117
Macbook Marconato <m.lucalmer@gmail.com>
2021-02-15 01:27:06 +0100

    added training for intervals

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
-from enum import Enum
+from enum import Enum, auto
 import inquirer
 from functools import partial
 import rtmidi
@@ -34,6 +34,15 @@ if a is not None:
     RUNNING_IN_PYCHARM = int(a)
 else:
     RUNNING_IN_PYCHARM = False
+
+Mode = Enum("Mode", "chord interval scale key")
+ChordType = Enum("ChordType", "triad extended_triad seventh")
+IntervalType = Enum("IntervalType", "diatonic chromatic")
+
+
+# class AutoName(Enum):
+#     def _generate_next_value_(name, start, count, last_values):
+#         return name
 
 
 # s.show('lily.svg')
@@ -72,8 +81,6 @@ def get_major_chords(random_inversion=True):
         should_be = f'{root[0] + root[2:].replace("-", "b")}-major triad'
         assert c.pitchedCommonName == should_be
     return chords
-
-
 
 
 def get_minor_chords(random_inversion=True):
@@ -115,23 +122,26 @@ major_chords = get_major_chords()
 minor_chords = get_minor_chords()
 
 
-def chord_training(chord_type, current_stack):
-    # address = chord.tables.ChordTableAddress(3, 11, -1, 2)
-    # iv = chord.tables.addressToIntervalVector(address)
-    # my_chord = chord.fromIntervalVector(iv)
-    # print(type(my_chord))
-    # print(my_chord.pitchedCommonName)
-    # print(chord_type)
-    random_chord = random.choice(minor_chords + major_chords)
+def wait_for_correct_answer(current_stack, correct_pitches):
+    while True:
+        time.sleep(0.03)
+        clone_stack = current_stack.copy()
+        current_stack_no_octave = set([c % 12 for c in clone_stack])
+        # print(current_stack_no_octave, correct_pitches)
+        if len(clone_stack) > 0:
+            time.sleep(0)
+            # print('ehila')
+        if clone_stack == correct_pitches:
+            return
+        # else:
+        #     print('NOT CORRECT')
+    # input("Press [enter] to continue.")
 
-    # chord.chordTables.Chor(address)
 
-    # a = f'C{np.random.random_integers(4, 7, 1).item()}'
-    # c_minor = chord.Chord([a, 'G4', 'E-5'])
-    random_chord.duration.type = "whole"
-    chord_name = random_chord.pitchedCommonName
+def plot_chord(my_chord, title):
+    my_chord.duration.type = "whole"
     path_grabber = PathGrabber()
-    random_chord.show("lily.png", my_show=False, my_path_grabber=path_grabber)
+    my_chord.show("lily.png", my_show=False, my_path_grabber=path_grabber)
     im = Image.open(path_grabber.path)
     x = np.array(im)
     black_pixels_012 = np.where(x != [255, 255, 255])
@@ -148,37 +158,57 @@ def chord_training(chord_type, current_stack):
     plt.box(False)
     ax.set_xticks([])
     ax.set_yticks([])
-    print(chord_name)
-    ax.set_title(chord_name)
+    ax.set_title(title)
     ax.imshow(x)
     plt.draw()
     plt.pause(0.001)
+
+    def make_green():
+        ax.imshow(green_x)
+        plt.draw()
+        plt.pause(0.001)
+
+    return ax, make_green
+
+
+def chord_training(chord_type, current_stack):
+    random_chord = random.choice(minor_chords + major_chords)
+    chord_name = random_chord.pitchedCommonName
+    ax, make_green = plot_chord(random_chord, title=chord_name)
     correct_pitches = set()
     for c in random_chord._notes:
         correct_pitches.add(c.pitch.pitchClass + 12 * c.octave)
-    while True:
-        time.sleep(0.03)
-        clone_stack = current_stack.copy()
-        current_stack_no_octave = set([c % 12 for c in clone_stack])
-        # print(current_stack_no_octave, correct_pitches)
-        if len(clone_stack) > 0:
-            time.sleep(0)
-            # print('ehila')
-        if clone_stack == correct_pitches:
-            if not RUNNING_IN_PYCHARM:
-                ax.imshow(green_x)
-                plt.draw()
-                plt.pause(0.001)
-            print("CORRECT!")
-            time.sleep(1)
-            break
-        # else:
-        #     print('NOT CORRECT')
-    # input("Press [enter] to continue.")
+    wait_for_correct_answer(current_stack, correct_pitches)
+    if not RUNNING_IN_PYCHARM:
+        make_green()
 
 
-Mode = Enum("Mode", "chord interval scale key")
-ChordType = Enum("ChordType", "triad extended_triad seventh")
+def interval_training(interval_type: str, current_stack):
+    c = random.sample('ABCDEFG', 1)[0]
+    s = f"{c}4"
+    print(s)
+    random_note = note.Note(s, quarterLength=1)
+    if interval_type == IntervalType.chromatic.name:
+        i = np.random.random_integers(-11, 11, 1)[0]
+    elif interval_type == IntervalType.diatonic.name:
+        i = random.sample([-10, -8, -7, -5, -3, -1, 2, 4, 5, 7, 9, 11], 1)[0]
+    else:
+        raise ValueError()
+    ii = interval.ChromaticInterval(i)
+    jj = ii.getDiatonic()
+    extra_name = f' ({jj.niceName})'
+
+    other_note = random_note.transpose(ii)
+    title = f'{s} {0 if i == 0 else ("+" if i > 0 else "-")}{abs(i)} semitones'f'{extra_name}'
+    my_chord = chord.Chord([random_note, other_note])
+    ax, make_green = plot_chord(my_chord, title=title)
+    ax.set_title(title)
+    correct_pitches = set()
+    for c in my_chord._notes:
+        correct_pitches.add(c.pitch.pitchClass + 12 * c.octave)
+    wait_for_correct_answer(current_stack, correct_pitches)
+    if not RUNNING_IN_PYCHARM:
+        make_green()
 
 
 def my_inquirer(questions):
@@ -211,6 +241,18 @@ def ask_chord_type():
             "answer",
             message="which chord type do you want to practice?",
             choices=[m.name for m in ChordType],
+        )
+    ]
+    answer = my_inquirer(questions)
+    return answer
+
+
+def ask_interval_type():
+    questions = [
+        inquirer.List(
+            "answer",
+            message="which interval type do you want to practice?",
+            choices=[m.name for m in IntervalType],
         )
     ]
     answer = my_inquirer(questions)
@@ -260,11 +302,16 @@ if __name__ == "__main__":
     in_port.set_callback(midi_in_callback, current_stack)
     exit = False
     while not exit:
-        mode = ask_mode()
+        if not RUNNING_IN_PYCHARM:
+            mode = ask_mode()
+        else:
+            mode = 'interval'
         if mode == "chord":
             chord_type = ask_chord_type()
             trainer = partial(chord_training, chord_type)
         elif mode == "interval":
+            interval_type = ask_interval_type()
+            trainer = partial(interval_training, interval_type)
             pass
         elif mode == "scale":
             pass
@@ -278,3 +325,4 @@ if __name__ == "__main__":
         except KeyboardInterrupt:
             print("back to main menu!")
             pass
+        time.sleep(1)
